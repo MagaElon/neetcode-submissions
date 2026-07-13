e6ae858991484d05b5abbc04724f0152047e2247
Bekzat Gulzhigit uulu <bekzatgulzhigituuluu@gmail.com>
2026-07-13 13:43:40 -0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        # Process each string once
+        for s in strs:
+            # Count frequencies for 'a' to 'z'
+            count = [0] * 26
+            # Build the frequency array for the current string
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            # Convert list to tuple so it can be used as a hash key
+            key = tuple(count)
+            # Group strings with the same frequency signature
+            groups[key].append(s)
+        # Return all grouped anagrams
+        return list(groups.values())
+        groups = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ird('a')] += 1
+
+            key = tuple(count)
+            groups[key].append(s)
+        return list(groups.values())
+        groups = defaultdict(list)
